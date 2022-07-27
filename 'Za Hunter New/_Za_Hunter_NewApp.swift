bde494bd10a6800eaeb00e942a965d0566463a57
Millan Bhandari <millan.ib@icloud.com>
2022-07-27 11:07:28 -0500

    Initial Commit

---
+++ 'Za Hunter New/_Za_Hunter_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_Hunter_NewApp.swift
+//  'Za Hunter New
+//
+//  Created by Millan Bhandari on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct _Za_Hunter_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
